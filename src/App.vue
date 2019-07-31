3cbba904199a41ecf015a79c154565600d7dcf5b
Shawn Barratt <mathus13@gmail.com>
2019-07-30 22:04:58 -0400

    basic site

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <citizen-up msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CitizenUp from "./components/CitizenUp.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    CitizenUp
   }
 };
 </script>
@@ -21,8 +20,9 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 30%;
+  margin-right: 30%;
 }
 </style>
